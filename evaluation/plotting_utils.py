033642dfcc1df58f34ab6a6de498bd98d4438cbe
Christian <c.foley.925@gmail.com>
2023-12-07 00:17:05 -0800

    updated plotting utils and generated paper figures

--- evaluation/plotting_utils.py
+++ evaluation/plotting_utils.py
@@ -1,8 +1,98 @@
 import seaborn as sns
 import numpy as np
+import string
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 from matplotlib.patches import Patch
+
+
+def measures_boxplot_close(
+    scores_db,
+    measures,
+    models,
+    labels,
+    colors,
+    measure_tags=[],
+    ylim=None,
+    show_outliers=False,
+    savef="",
+    title="",
+):
+    """
+    Generates a box plot comparing different models across various measures.
+    Creates a box plot for each model, displaying the distribution of scores for each measure.
+    Parameters:
+    scores_db (dict): A nested dictionary where the first key is the measure, the second key is the model,
+                      and the value is a list of scores.
+    measures (list of str): A list of measures (e.g., 'Accuracy', 'Precision') to be plotted.
+    models (list of str): A list of model names corresponding to keys in scores_db.
+    colors (list of str): A list of colors to be used for each model's box plot.
+    ylim (tuple of float, optional): A tuple specifying the y-axis limits (min, max).
+                    If None, the limits are set automatically.
+    Returns:
+    None: This function creates and displays a matplotlib plot but does not return any value.
+    """
+    sns.set_style("whitegrid")
+    num_measures = len(measures)
+    box_width = 0.8  # Width of each boxplot
+    spacing = 1.6  # Spacing between groups of boxplots
+    positions = [
+        np.arange(len(models)) * (num_measures + spacing) + i
+        for i in range(num_measures)
+    ]
+
+    fig, ax = plt.subplots(figsize=(max(12, 3.5 * len(models)), 6), dpi=150)
+
+    for i, measure in enumerate(measures):
+        # Plotting boxplots
+        data = [scores_db[measure][model] for model in models]
+        boxplot_elements = ax.boxplot(
+            data,
+            positions=positions[i],
+            widths=box_width,
+            patch_artist=True,
+            boxprops=dict(alpha=0.6),
+            medianprops=dict(color="black"),
+            showfliers=show_outliers,
+        )
+
+        # Setting the same color for all boxes of a measure
+        for patch, color in zip(boxplot_elements["boxes"], colors):
+            patch.set_facecolor(color)
+
+        # Adding letter labeling
+        if measure_tags:
+            letter = measure_tags[i]
+        else:
+            letter = measure[:2]
+        for pos in positions[i]:
+            ax.text(
+                pos,
+                -1.3,
+                letter,
+                ha="center",
+                va="top",
+                fontsize=9,
+                # fontweight="bold",
+            )
+
+    # Adjusting x-axis labels and positions
+    ax.set_xticks(
+        np.arange(len(models)) * (num_measures + spacing) + num_measures / 2 - 0.5
+    )
+    ax.set_xticklabels(labels, fontsize=14)
+
+    ax.set_ylabel("Projected Scores", fontsize=14)
+    ax.set_title(
+        "Model Performance Comparison by Measure" if not title else title, fontsize=16
+    )
+
+    if ylim is not None:
+        ax.set_ylim(*ylim)
+
+    if savef:
+        plt.savefig(savef, facecolor=(1, 1, 1), dpi=100)
+    plt.show()
 
 
 def measures_boxplot(
@@ -14,6 +104,7 @@ def measures_boxplot(
     ylim=None,
     show_outliers=False,
     savef="",
+    measure_tags=[],
 ):
     """
     Generates a box plot comparing different models across various measures.
@@ -58,8 +149,8 @@ def measures_boxplot(
         # overlaying text
         for j, pos in enumerate(positions[i]):
             med_val = np.median(data[j])
-            x_position = pos - 0.36
-            y_position = med_val
+            x_position = pos - 0.4
+            y_position = min(med_val, 95)
             ax.text(
                 x_position,
                 y_position,
@@ -78,12 +169,18 @@ def measures_boxplot(
     ax.legend(handles=legend_patches, loc="upper left", bbox_to_anchor=(1, 1))
     plt.subplots_adjust(right=0.75)
     ax.set_ylabel("Scores")
-    ax.set_title("Model Performance Comparison", fontsize=16)
+    ax.set_title(
+        "Model Performance Comparison: " + " ".join(measure_tags)
+        if measure_tags
+        else "",
+        fontsize=16,
+    )
 
     if ylim is not None:
         ax.set_ylim(*ylim)
 
     if savef:
+        plt.tight_layout()
         plt.savefig(savef, facecolor=(1, 1, 1), dpi=100)
     plt.show()
 
